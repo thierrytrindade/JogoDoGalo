34e4e18e37ffa6125588b58ddf24df7792b8932d
datrindadejeremy <datrindadejeremy@gmail.com>
2025-03-03 01:27:58 +0000

    cores escolhidas e organizadas alfabeticamente

---
+++ coes_escolhidas.py
@@ -0,0 +1,13 @@
+Enumere a cor que deseja utilizar para os seus simbolos
+
+1	 - 	AMARELO = "\033[1;33m"
+2	 - 	AZUL = "\033[1;34m"
+3	 - 	BRANCO = "\033[1;37m"
+4	 - 	CASTANHO = "\033[0;33m"
+5	 - 	CIANO = "\033[0;36m"
+6	 - 	CINZENTO = "\033[1;30m"
+7	 - 	PRETO = "\033[0;30m"
+8	 - 	ROXO = "\033[1;35m"
+9	 - 	VERDE = "\033[1;32m"
+10	 - 	VERMELHO = "\033[1;31m"
+
---
+++ verificacao_de_cores.py
@@ -0,0 +1,35 @@
+
+VERMELHO = "\033[0;31m"
+VERDE = "\033[0;32m"
+AZUL = "\033[0;34m"
+ROXO = "\033[0;35m"
+CIANO = "\033[0;36m"
+CINZENTO_CLARO = "\033[0;37m"
+CINZENTO_ESCURO = "\033[1;30m"
+VERMELHO_CLARO = "\033[1;31m"
+VERDE_CLARO = "\033[1;32m"
+AZUL_CLARO = "\033[1;34m"
+ROXO_CLARO = "\033[1;35m"
+CIANO_CLARO = "\033[1;36m"
+END = "\033[0m"  #Anula a formatação anterior
+
+
+
+
+
+
+print(CIANO + "033[0;36m" + END)
+print(CINZENTO_ESCURO + "033[1;30m" + END)
+print(VERMELHO_CLARO + "033[1;31m" + END)
+print(VERDE_CLARO + "033[1;32m" + END)
+print(AZUL_CLARO + "033[1;34m" + END)
+print(ROXO_CLARO + "033[1;35m" + END)
+
+
+
+
+
+
+
+
+
