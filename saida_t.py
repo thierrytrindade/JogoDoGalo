fb37211f71720791526d921357a22a5c0077a87b
thierrytrindade <thierrytrindade2007@gmail.com>
2025-03-06 22:00:12 +0000

    Título a negrito

--- saida_t.py
+++ saida_t.py
@@ -1,4 +1,14 @@
 #Saida
+PREDEFENIDO = ""
+AMARELO = "\033[1;33m"   
+AZUL = "\033[1;34m"      
+BRANCO = "\033[1;37m"    
+CIANO = "\033[0;36m"     
+ROXO = "\033[1;35m"      
+VERDE = "\033[1;32m"     
+VERMELHO = "\033[1;31m"  
+END = "\033[0m"  #Anula a formatação anterior
+NEGRITO="\033[1m"
 largura_total = 60
 
 print("\t╔" + "═" * (largura_total - 2) + "╗")
@@ -44,7 +54,7 @@ pVoltar ="\t║" +"        9 - Voltar ao menu anterior       ".center(largura_to
 espaco = "\n"*3
 print(espaco)
 print("\t╔" + "═" * (largura_total - 2) + "╗") 
-print("\t║" +"JOGO DO GALO".center(largura_total - 2,".") + "║")
+print("\t║" +(NEGRITO+"JOGO DO GALO"+END).center(largura_total - 2,".") + "║")
 print("\t║" +"      1 - Jogar       ".center(largura_total - 2,".") + "║")
 print("\t║" +"   2 - Personalizar   ".center(largura_total - 2,".") + "║")
 print(pSair)
@@ -57,10 +67,31 @@ print("\t╚" + "═" * (largura_total - 2) + "╝")
 #menu2
 print(espaco)
 print("\t╔" + "═" * (largura_total - 2) + "╗") 
-print("\t║" + " => Jogar ".center(largura_total - 2,".") + "║")
+print("\t║" + (NEGRITO+" => Jogar "+END).center(largura_total - 2,".") + "║")
 print("\t║" + "      1 - Partida simples/indefinida      ".center(largura_total - 2,".") + "║")
 print("\t║" + ".              2 - Melhor de 3             ".center(largura_total - 2,".") + "║")
 print("\t║" + ".              3 - Melhor de 5             ".center(largura_total - 2,".") + "║")
 print("\t║" + "    4 - Personalizar o número de jogos    ".center(largura_total - 2,".") + "║")
+print(pVoltar)
+print("\t╚" + "═" * (largura_total - 2) + "╝")
+
+
+
+#Menu 3
+jogador1="Velcro"
+jogador2="kmkdnkfn"
+update="jbdusgaofug"
+
+
+print(espaco)
+print(AMARELO+update+END)
+print("\t╔" + "═" * (largura_total - 2) + "╗") 
+
+print("\t║" + (NEGRITO+" => Personalizar "+END).center(largura_total - 2,".") + "║")
+print("\t║" + ("1 - Mudar nome do(a) " + jogador1).center(largura_total - 2,".") + "║" )
+print("\t║" + ("2 - Mudar cor do(a) " + jogador1).center(largura_total - 2,".") + "║" )
+print("\t║" + ("3 - Mudar nome do(a) " + jogador2).center(largura_total - 2,".") + "║" )
+print("\t║" + ("4 - Mudar cor do(a) " + jogador2).center(largura_total - 2,".") + "║" )
+print("\t║" + "5 - Mudar Simbolo".center(largura_total - 2,".") + "║" )
 print(pVoltar)
 print("\t╚" + "═" * (largura_total - 2) + "╝")
