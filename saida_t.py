c615148386f3548fc3ffa142b28fcf505ddcaf16
thierrytrindade <thierrytrindade2007@gmail.com>
2025-03-06 20:08:57 +0000

    Saida criativa feita

---
+++ saida_t.py
@@ -0,0 +1,31 @@
+#Saida
+largura_total = 60
+
+print("\t╔" + "═" * (largura_total - 2) + "╗")
+print("\t║" + "🚀 O JOGO MAIS ÉPICO DE SEMPRE! 🚀".center(largura_total - 4) + "║")
+print("\t║" + "🎮  Desenvolvido por TT Games 🎮".center(largura_total - 4) + "║")
+print("\t╠" + "═" * (largura_total - 2) + "╣")
+print("\t║" + "🌟 Equipe de Desenvolvimento 🌟".center(largura_total - 4) + "║")
+print("\t╠" + "═" * (largura_total - 2) + "╣")
+print("\t║" + "Thierry 'Chefe Supremo' Trindade".center(largura_total - 2) + "║")
+print("\t║" + "Santiago 'O' Santos".center(largura_total - 2) + "║")
+print("\t║" + "Andre 'Ratazana' Madail".center(largura_total - 2) + "║")
+print("\t╠" + "═" * (largura_total - 2) + "╣")
+print("\t║" + "🕵️  Testadores".center(largura_total - 1) + "║")
+print("\t║" + "Professor com paciência infinita".center(largura_total - 2) + "║")
+print("\t╠" + "═" * (largura_total - 2) + "╣")
+print("\t║" + "🛠️  Engine & Ferramentas Utilizadas".center(largura_total - 1) + "║")
+print("\t║" + "Desenvolvido em Python 🐍".center(largura_total - 3) + "║")
+print("\t║" + "Google & CTRL+C / CTRL+V".center(largura_total - 2) + "║")
+print("\t╠" + "═" * (largura_total - 2) + "╣")
+print("\t║" + "🙌  Agradecimentos Especiais".center(largura_total - 3) + "║")
+print("\t║" + "Google, Wikipedia e tutoriais infinitos".center(largura_total - 2) + "║")
+print("\t║" + "CTRL+Z, salvando vidas desde sempre".center(largura_total - 2) + "║")
+print("\t║" + "Impulsionado por café e ansiedade! ☕😖".center(largura_total - 4) + "║")
+print("\t╠" + "═" * (largura_total - 2) + "╣")
+print("\t║" + "🎖️    Mensagem Final     ".center(largura_total - 1) + "║")
+print("\t║" + "\"Obrigado por jogar e aguentar os bugs!\"".center(largura_total - 2) + "║")
+print("\t║" + "Sair...".center(largura_total - 2) + "║")
+print("\t╚" + "═" * (largura_total - 2) + "╝")#Texto[Aquele que quero centralizar].center(largura[tamanho da largura total menos as bordas e tambem os emogis, pois ocupam mais de um espaço], caractere_de_preenchimento[Tem como predefenido o espaço se caso de não preenchimento]).
+#retirei os emogis do link:https://en.wikipedia.org/wiki/List_of_emojis
+
